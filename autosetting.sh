46ad693e6762c170d4a450df33e6f887a93bf550
Guest <guest-nze4ro@h20490.sgb.skir.kr>
2021-08-16 23:16:01 +0900

    good

--- autosetting.sh
+++ autosetting.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+# This file should be sourced 
 chmod +x autosetting.sh
 
 PATH=$PATH:~/.local/bin
 python get-pip.py
 pip install virtualenv
 virtualenv myproject
-
-# 이 아래 코드는 자동으로 실행이 안되는데 왜 안될까요...
 cd projects
 source ~/python_back-end/myproject/bin/activate
+pip install flask
---
+++ projects/myproject/autoflask.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# 반드시 myproject안에서 실행할 것
+
+FLASK_APP=pybo FLASK_ENV=development flask run
